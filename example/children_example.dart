5d1c05625db686b4ce047ace87ca830847ba4fef
George Cheng <Gerhut@GMail.com>
2019-07-03 16:31:18 +0800

    dartfmt

--- example/children_example.dart
+++ example/children_example.dart
@@ -1,7 +1,16 @@
 import 'package:children/children.dart';
 
 main() {
-  var originalList = ["There", "should", "not", "be", "any", null, "in", "list"];
+  var originalList = [
+    "There",
+    "should",
+    "not",
+    "be",
+    "any",
+    null,
+    "in",
+    "list"
+  ];
   var filteredList = children(originalList);
   print('originalList: ${originalList}');
   print('filteredList: ${filteredList}');
--- lib/src/children_base.dart
+++ lib/src/children_base.dart
@@ -1,2 +1,2 @@
-List<T> children<T>(List<T> list, { bool growable = false }) =>
-  list.whereType<T>().toList(growable: growable);
+List<T> children<T>(List<T> list, {bool growable = false}) =>
+    list.whereType<T>().toList(growable: growable);
